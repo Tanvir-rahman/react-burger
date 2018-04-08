c8c6d0dedeacb010b47ebb9a1746dc03367337b1
Tanvir-rahman <rahmantanvir25558@gmail.com>
2018-04-08 16:53:01 +0600

    connecting state to build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/_Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
 
 class BurgerBuilder extends Component {
     // constructor(props) {
@@ -14,14 +22,23 @@ class BurgerBuilder extends Component {
             bacon: 1,
             cheese: 2,
             meat: 3
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        
+    }
+
+    removeIngredientHandler = (type) => {
+
     }
 
     render () {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <BuildControls/>
             </Aux>
         );
     }
